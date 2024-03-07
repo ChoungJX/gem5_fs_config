463debc65f588ae179559d4432446287b820b11a
ChoungJX <zhenglinfeng43@gmail.com>
2024-03-07 19:34:23 +0900

    create script for web server simulation

--- data/init_fast.sh
+++ data/init_fast.sh
@@ -15,7 +15,7 @@
 # /tmp/run.sh
 # cat "boot success"
 # service postgresql start
-pg_ctlcluster 12 main start
+pg_ctlcluster 10 main start
 /bin/hostname database
 /sbin/ifconfig eth0 hw ether 00:90:00:00:00:05
 /sbin/ifconfig eth0 192.168.0.5 netmask 255.255.255.0 up
--- run_sw.sh
+++ run_sw.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./build/ARM/gem5.fast -d m5out/switch configs/dist/sw.py --is-switch --checkpoint-dir=m5out/switch --dist-server-port=2200 --dist-size=21 --dist-sync-repeat="10us" --dist-sync-start="1000000000000t"
+./build/ARM/gem5.fast -d m5out/switch configs/dist/sw.py --is-switch --checkpoint-dir=m5out/switch --dist-server-port=2200 --dist-size=3 --dist-sync-repeat="10us" --dist-sync-start="1000000000000t"
