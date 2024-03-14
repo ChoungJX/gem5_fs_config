c109c63fa79c55f2374f2c85ea9031a6911e34d4
ChoungJX <zhenglinfeng43@gmail.com>
2024-03-14 19:55:28 +0900

    remove

--- data/web/init_server.sh
+++ data/web/init_server.sh
@@ -3,7 +3,7 @@
 # m5 checkpoint 1
 # sleep 2
 
-export ACTIX_TECHEMPOWER_MONGODB_URL="mongodb://192.168.0.5:27017"
+# export ACTIX_TECHEMPOWER_MONGODB_URL="mongodb://192.168.0.5:27017"
 m5 workbegin
 sleep 1
 m5 readfile > /tmp/workload
