df87aca58f5f1fff8143a0ad570bd4d7752325b4
ChoungJX <zhenglinfeng43@gmail.com>
2024-03-14 21:06:08 +0900

    add ;

--- data/web/init_client.sh
+++ data/web/init_client.sh
@@ -1,15 +1,15 @@
 
-m5 workbegin
-sleep 1
-m5 readfile > /tmp/workload
-chmod 755 /tmp/workload
+m5 workbegin;
+sleep 1;
+m5 readfile > /tmp/workload;
+chmod 755 /tmp/workload;
 
-/bin/hostname client
-/sbin/ifconfig eth0 hw ether 00:90:00:00:00:06
-/sbin/ifconfig eth0 192.168.0.6 netmask 255.255.255.0 up
-/sbin/ifconfig -a
+/bin/hostname client;
+/sbin/ifconfig eth0 hw ether 00:90:00:00:00:06;
+/sbin/ifconfig eth0 192.168.0.6 netmask 255.255.255.0 up;
+/sbin/ifconfig -a;
 
-echo "finish"
-sleep 3
-curl http://192.168.0.4:3000
-/tmp/workload -t1 -c100 -d5s http://192.168.0.4:3000
+echo "finish";
+sleep 3;
+curl http://192.168.0.4:3000;
+/tmp/workload -t1 -c100 -d5s http://192.168.0.4:3000;
--- data/web/init_server.sh
+++ data/web/init_server.sh
@@ -4,15 +4,15 @@
 # sleep 2
 
 # export ACTIX_TECHEMPOWER_MONGODB_URL="mongodb://192.168.0.5:27017"
-m5 workbegin
-sleep 1
-m5 readfile > /tmp/workload
-chmod 755 /tmp/workload
+m5 workbegin;
+sleep 1;
+m5 readfile > /tmp/workload;
+chmod 755 /tmp/workload;
 
-/bin/hostname server0
-/sbin/ifconfig eth0 hw ether 00:90:00:00:00:04
-/sbin/ifconfig eth0 192.168.0.4
-/sbin/ifconfig -a
-echo "finish"
+/bin/hostname server0;
+/sbin/ifconfig eth0 hw ether 00:90:00:00:00:04;
+/sbin/ifconfig eth0 192.168.0.4;
+/sbin/ifconfig -a;
+echo "finish";
 
-/tmp/workload
+/tmp/workload;
