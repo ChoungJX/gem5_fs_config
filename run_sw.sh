776cf715319f52fc758ed750ac1039fef2bb4dc0
ChoungJX <zhenglinfeng43@gmail.com>
2024-03-17 15:32:00 +0900

    modified

--- run_sw.sh
+++ run_sw.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "use port: $1"
-./build/ARM/gem5.fast -d m5out/switch configs/dist/sw.py --is-switch --checkpoint-dir=m5out/switch --dist-server-port=$1 --dist-size=2 --dist-sync-repeat="10us" --dist-sync-start="10t"
+echo "log file is in m5out/switch_$1"
+./build/ARM/gem5.fast -d m5out/switch configs/dist/sw.py --is-switch --checkpoint-dir=m5out/switch_$1 --dist-server-port=$1 --dist-size=2 --dist-sync-repeat="10us" --dist-sync-start="10t"
