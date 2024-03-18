121b2d5c1a27217d30c999a7c29b3cac9badf582
ChoungJX <zhenglinfeng43@gmail.com>
2024-03-18 23:40:40 +0900

    modified

--- data/web/init_client.sh
+++ data/web/init_client.sh
@@ -1,18 +1,29 @@
 
 m5 workbegin;
+
 sleep 1;
+
 m5 readfile > /tmp/workload;
+
 chmod 755 /tmp/workload;
 
 /bin/hostname client;
+
 /sbin/ifconfig eth0 hw ether 00:90:00:00:00:06;
+
 /sbin/ifconfig eth0 192.168.0.6 netmask 255.255.255.0 up;
+
 /sbin/ifconfig -a;
 
 
 sleep 3;
+
 curl http://192.168.0.4:3000;
+
 echo "finished curl test";
+
 sleep 1;
+
 echo "start to run wrk";
+
 /tmp/workload -t8 -c800 -d5s http://192.168.0.4:3000;
--- data/web/init_server.sh
+++ data/web/init_server.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# m5 checkpoint 1
-# sleep 2
 
-# export ACTIX_TECHEMPOWER_MONGODB_URL="mongodb://192.168.0.5:27017"
 m5 workbegin;
+
 sleep 1;
+
 m5 readfile > /tmp/workload;
+
 chmod 755 /tmp/workload;
 
 /bin/hostname server0;
+
 /sbin/ifconfig eth0 hw ether 00:90:00:00:00:04;
+
 /sbin/ifconfig eth0 192.168.0.4;
+
 /sbin/ifconfig -a;
-echo "finish";
+
+echo "finish configuration";
+
+echo "start to run workload";
 
 /tmp/workload;
