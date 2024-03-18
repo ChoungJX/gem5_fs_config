e35090bb4577fe14ab115d67beeb3c7705ef1c22
ChoungJX <zhenglinfeng43@gmail.com>
2024-03-19 00:19:34 +0900

    modified

--- data/web/init_client.sh
+++ data/web/init_client.sh
@@ -1,29 +1,30 @@
 
-m5 workbegin;
+sleep 1
 
-sleep 1;
+m5 workbegin
 
-m5 readfile > /tmp/workload;
+sleep 1
 
-chmod 755 /tmp/workload;
+m5 readfile > /tmp/workload
 
-/bin/hostname client;
+chmod 755 /tmp/workload
 
-/sbin/ifconfig eth0 hw ether 00:90:00:00:00:06;
+/bin/hostname client
 
-/sbin/ifconfig eth0 192.168.0.6 netmask 255.255.255.0 up;
+/sbin/ifconfig eth0 hw ether 00:90:00:00:00:06
+/sbin/ifconfig eth0 192.168.0.6 netmask 255.255.255.0 up
 
-/sbin/ifconfig -a;
+/sbin/ifconfig -a
 
 
-sleep 3;
+sleep 3
 
-curl http://192.168.0.4:3000;
+curl http://192.168.0.4:3000
 
-echo "finished curl test";
+echo "finished curl test"
 
-sleep 1;
+sleep 1
 
-echo "start to run wrk";
+echo "start to run wrk"
 
-/tmp/workload -t8 -c800 -d5s http://192.168.0.4:3000;
+/tmp/workload -t8 -c800 -d5s http://192.168.0.4:3000
--- data/web/init_server.sh
+++ data/web/init_server.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
+sleep 1
 
-m5 workbegin;
+m5 workbegin
 
-sleep 1;
+sleep 1
 
-m5 readfile > /tmp/workload;
+m5 readfile > /tmp/workload
 
-chmod 755 /tmp/workload;
+chmod 755 /tmp/workload
 
-/bin/hostname server0;
+/bin/hostname server0
 
-/sbin/ifconfig eth0 hw ether 00:90:00:00:00:04;
+/sbin/ifconfig eth0 hw ether 00:90:00:00:00:04
 
-/sbin/ifconfig eth0 192.168.0.4;
+/sbin/ifconfig eth0 192.168.0.4
 
-/sbin/ifconfig -a;
+/sbin/ifconfig -a
 
-echo "finish configuration";
+echo "finish configuration"
 
-echo "start to run workload";
+echo "start to run workload"
 
-/tmp/workload;
+/tmp/workload
