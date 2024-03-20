655744203405772df58dde3b4b927fe47fd9e18b
ChoungJX <zhenglinfeng43@gmail.com>
2024-03-20 21:16:34 +0900

    modified

---
+++ data/init_single.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+m5 workbegin
+sleep 1
+m5 readfile > /tmp/server
+chmod 755 /tmp/server
+
+
+m5 workbegin
+sleep 1
+m5 readfile > /tmp/wrk
+chmod 755 /tmp/wrk
+
+echo "start to boot server"
+nohup /tmp/server > /dev/null 2>&1 &
+
+echo "test server status"
+curl http://127.0.0.1:3000
+
+echo "finished boot server"
+
+echo "start to boot wrk"
+/tmp/wrk -t1 -c200 -d1s http://127.0.0.1:3000
