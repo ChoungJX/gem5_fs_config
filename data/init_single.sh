900579328b991f8dd322d4339c5168470687a916
ChoungJX <zhenglinfeng43@gmail.com>
2024-03-27 21:27:16 +0900

    modified

--- data/init_single.sh
+++ data/init_single.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+service haveged start
 m5 workbegin
-sleep 1
+
+echo "load server binary"
 m5 readfile > /tmp/server
 chmod 755 /tmp/server
 
-
+echo "set server ip"
 ifconfig lo 127.0.0.1
 /sbin/ifconfig -a
 
+echo "load wrk"
 m5 workbegin
-sleep 1
 m5 readfile > /tmp/wrk
 chmod 755 /tmp/wrk
 
+m5 workbegin
+echo "load database file"
+m5 readfile > /root/hello_world.db
+
 echo "start to boot server"
-nohup /tmp/server > /dev/null 2>&1 &
+DATABASE_URL=/root/hello_world.db nohup /tmp/server > /dev/null 2>&1 &
 
 echo "test server status"
-sleep 3
 curl http://127.0.0.1:3000
 
 echo "finished boot server"
